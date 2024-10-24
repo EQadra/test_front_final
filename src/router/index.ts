3d1785baf2b72e1a0f77c8587d593e7cb0f46b23
Eduardo Cuadra <ecuadra06@gmail.com>
2024-10-24 06:00:51 -0500

    Creacion de router para funcipnalidad

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import CharacterList from '../views/CharacterList.vue'
+import CharacterDetail from '../views/CharacterDetail.vue'
+import CharacterChapters from '../views/CharacterChapters.vue'
+import OpenAIChat from '../views/OpenAIChat.vue'
+import Dashboard from '../views/Dashboard.vue'
 
+// Definición de las rutas con tipo RouteRecordRaw
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', component: Login },
+  { path: '/register', component: Register },
+  { path: '/dashboard', component: Dashboard },
+  { path: '/characters/', component: CharacterList },
+  { path: '/characters/:id', component: CharacterDetail },
+  { path: '/characters/:id/chapters', component: CharacterChapters },
+  { path: '/openai', component: OpenAIChat },
+]
+
+// Creación del router con el historial web
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
